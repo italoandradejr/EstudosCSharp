7ef221aa966d0d228f48edd8d5920dbe8c7eca3b
italoandradejr <italoandradejr@gmail.com>
2023-07-23 19:32:12 -0300

    Fianlizando curso CSharp

--- ScreenSound_04/ScreenSound_04/Program.cs
+++ ScreenSound_04/ScreenSound_04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ScreenSound_04.Modelos;
 using System.Text.Json;
+using ScreenSound_04.Filtros;
 
 using (HttpClient client = new HttpClient())
 {
@@ -8,7 +9,27 @@ using (HttpClient client = new HttpClient())
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         //Console.WriteLine(resposta);
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-        musicas[0].ExibirDetalhesDaMusica();
+
+        LinqFilter.FiltrarMusicaEmCSharp(musicas);
+        
+        //musicas[1].ExibirDetalhesDaMusica();
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
+        //LinqFilter.FiltrarMusicasdeUmArtista(musicas, "Michel Teló");
+
+
+        //var musicasPreferidasDoDaniel = new MusicasPreferidas("Daniel");
+
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[377]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[4]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[6]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1467]);
+
+        //musicasPreferidasDoDaniel.ExbirMusicasFavoritas();
+
+        //musicasPreferidasDoDaniel.GerarArquivoJson();
     }
     catch (Exception ex)
     {
