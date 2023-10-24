662b4f69b6f76686cf50cf0dfec88004df054ac1
Italo Andrade <italoandradejr@gmail.com>
2023-10-24 17:23:50 -0300

    arquivosC

--- ByteBankIO-master/ByteBankIO/Program.cs
+++ ByteBankIO-master/ByteBankIO/Program.cs
@@ -6,25 +6,33 @@ class Program
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
 
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-        var buffer = new byte[1024]; // 1KB
-                
-        while (numeroDeBytesLidos != 0)
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
-        }
+            var numeroDeBytesLidos = -1;
+                       
+            var buffer = new byte[1024]; // 1KB
 
-        Console.ReadLine();
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                Console.WriteLine("\n=====================================");
+                Console.WriteLine($"Bytes lidos: {numeroDeBytesLidos}");
+                Console.WriteLine("=====================================");
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+            }
+
+            fluxoDoArquivo.Close();
+            Console.ReadLine();
+        }
     }
 
-    static void EscreverBuffer(byte[] buffer)
+            
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
     {
         var utf8 = new UTF8Encoding();
 
-        var texto = utf8.GetString(buffer);
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
         Console.Write(texto);
 
         /*
