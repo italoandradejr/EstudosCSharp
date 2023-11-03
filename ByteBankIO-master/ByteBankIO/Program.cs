d175cd2ffef8263c1241a01e7793220cabdb22f5
Italo Andrade <italoandradejr@gmail.com>
2023-11-03 13:03:27 -0300

    modulo_finalizado

--- ByteBankIO-master/ByteBankIO/Program.cs
+++ ByteBankIO-master/ByteBankIO/Program.cs
@@ -6,8 +6,7 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        EscritaBinaria();
-        LeituraBinaria();
+        UsarStreamDeEntrada();
 
         Console.WriteLine("Aplicação finalizada..");
 
@@ -29,3 +28,25 @@ using (var escritor = new StreamWriter(fluxoDeArquivo))
 
 Console.WriteLine("Aplicação finalizada...");
 */
+
+//Console.WriteLine("Digite seu nome:");
+//var nome = Console.ReadLine();
+
+//var linhas = File.ReadAllLines("contas.txt");
+//Console.WriteLine(linhas.Length);
+
+/*
+foreach (var linha in linhas)
+{
+    Console.WriteLine(linha);
+}
+*/
+
+//var bytesArquivo = File.ReadAllBytes("contas.txt");
+//Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+//File.WriteAllText("escrevendoComClasseFile.txt", "Testando File.WriteAllText");
+
+//Console.WriteLine("Aplicação Finalizada ...");
+
+//Console.ReadLine();
